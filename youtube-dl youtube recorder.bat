350515165f4d2740a73f61efe02cc6c13d8b1574
razor91111 <66380989+razor91111@users.noreply.github.com>
2021-05-03 22:30:31 +0300

    Update and rename youtube recorder.bat to youtube-dl youtube recorder.bat

--- youtube-dl youtube recorder.bat
+++ youtube-dl youtube recorder.bat
@@ -1,15 +1,11 @@
 :loop
 @echo off
 set name=%NASASpaceflight
-set name=%name: =0%
 set token=%UCSUu1lih2RifWkKtDOJdsBA
-set token=%token: =0%
 set DATE_TIME=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
-set DATE_TIME=%DATE_TIME: =0%
 set /A clock=%RANDOM% %% 80 + 1
 set clock=%clock: =0%
 Echo on
 youtube-dl -r 3M --download-archive dl.txt --playlist-end 1 --hls-use-mpegts --no-part https://www.youtube.com/channel/%token%/live -o %name%_%DATE_TIME%.ts
-
 timeout /t %clock%
 goto loop
