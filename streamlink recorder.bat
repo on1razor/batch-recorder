f2168298576f462a14b30c169b4dd4ac2c6ed150
razor91111 <66380989+razor91111@users.noreply.github.com>
2021-05-03 22:13:43 +0300

    Update streamlink recorder.bat

--- streamlink recorder.bat
+++ streamlink recorder.bat
@@ -3,7 +3,7 @@
 set name=%shroud
 set name=%name: =0%
 echo on
-streamlink --hls-segment-threads 5 --retry-streams 40 -o "%name%.ts" https://www.twitch.tv/%name% best
+streamlink --twitch-disable-ads --hls-segment-threads 5 --retry-streams 40 -o "%name%.ts" https://www.twitch.tv/%name% best
 @echo off
 set DATE_TIME=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
 set DATE_TIME=%DATE_TIME: =0%
