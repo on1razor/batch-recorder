0e5f2916edd2be66afbb3b79f2065d9d6bb17c3c
razor91111 <66380989+razor91111@users.noreply.github.com>
2021-05-03 22:26:38 +0300

    Update and rename twitch recorder.bat to youtube-dl twitch recorder.bat

--- youtube-dl twitch recorder.bat
+++ youtube-dl twitch recorder.bat
@@ -1,13 +1,10 @@
 :loop
 @echo off
 set name=%shroud
-set name=%name: =0%
 set DATE_TIME=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
-set DATE_TIME=%DATE_TIME: =0%
 set /A clock=%RANDOM% %% 60 + 1
 set clock=%clock: =0%
 Echo on
-youtube-dl -r 3M --download-archive dl.txt --playlist-end 1 --hls-use-mpegts --no-part https://www.twitch.tv/%name% -o %name%_%DATE_TIME%.ts
-
+youtube-dl --hls-use-mpegts --no-part https://www.twitch.tv/%name% -o %name%_%DATE_TIME%.ts
 timeout /t %clock%
 goto loop
